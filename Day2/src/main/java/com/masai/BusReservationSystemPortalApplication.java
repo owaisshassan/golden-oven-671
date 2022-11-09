b8844b5b1acab310cae274919e4c1205647df555
OwaissHassan <khabarquss@gmail.com>
2022-11-09 10:10:50 +0530

    model

---
+++ Day2/src/main/java/com/masai/BusReservationSystemPortalApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusReservationSystemPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusReservationSystemPortalApplication.class, args);
+	}
+
+}
