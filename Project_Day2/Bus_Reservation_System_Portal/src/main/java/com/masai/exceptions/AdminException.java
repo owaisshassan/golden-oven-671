984d51218fa5237e855751ab487fa1314bcc63fb
Samson David Seemal <99979021+SamsonSeemal@users.noreply.github.com>
2022-11-10 18:22:13 +0530

    Day 3
    Task and ISsues done for the Day

---
+++ Project_Day2/Bus_Reservation_System_Portal/src/main/java/com/masai/exceptions/AdminException.java
@@ -0,0 +1,15 @@
+package com.masai.exceptions;
+
+public class AdminException extends RuntimeException{
+	
+	public AdminException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public AdminException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+
+}
---
+++ Project_Day2/Bus_Reservation_System_Portal/src/main/java/com/masai/repository/CurrentAdminSessionRepo.java
@@ -0,0 +1,12 @@
+package com.masai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masai.model.CurrentAdminSession;
+
+public interface CurrentAdminSessionRepo extends JpaRepository<CurrentAdminSession, Integer>{
+	
+	public  CurrentAdminSession  findByUuid(String uuid);
+
+}
